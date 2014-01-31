e2f0344a7b36d3a5a9917077656bf5b5c935d6b8
vagrant <vagrant@precise32.(none)>
2014-01-31 19:19:49 +0000

    testing index.js

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@
 exports.view = function(req, res){
 res.render('index', {
     'projects': [
-      { 'name': 'Waiting in Line',
+      { 'name': 'Waiting in Line Test',
         'image': 'lorempixel.people.1.jpeg',
         'id': 'project1'
       },
